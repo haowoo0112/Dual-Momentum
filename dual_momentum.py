73081aa526b4dec2dbb0088bce155465d07f6815
haowoo0112 <haowoo0112@gmail.com>
2022-08-30 17:06:12 +0800

    feat: add database which can store price data

--- dual_momentum.py
+++ dual_momentum.py
@@ -6,13 +6,10 @@ import sqlite3
 
 def find_closing_price_TWSE(stock_number, year, month):
     http = urllib3.PoolManager()
-    url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY_AVG?response=html&date='+year+month+'01&stockNo='+stock_number
-    try: 
-        res = http.request('GET', url)
-        html_doc = res.data
-        res.close()
-    except requests.exceptions.RequestException as e:
-        print(e)
+    url = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY_AVG?response=html&date='+year+month+'01&stockNo='+stock_number 
+    res = http.request('GET', url)
+    html_doc = res.data
+    res.close()
 
 
     soup = BeautifulSoup(html_doc, 'html.parser')
@@ -26,12 +23,10 @@ def find_closing_price_TWSE(stock_number, year, month):
 def find_closing_price_TPEX(stock_number, year, month):
     http = urllib3.PoolManager()
     url = 'https://www.tpex.org.tw/web/stock/aftertrading/daily_trading_info/st43_print.php?l=zh-tw&d='+year+'/'+month+'&stkno='+stock_number+'&s=0,asc,0'
-    try: 
-        res = http.request('GET', url)
-        html_doc = res.data
-        res.close()
-    except requests.exceptions.RequestException as e:
-        print(e)
+    res = http.request('GET', url)
+    html_doc = res.data
+    res.close()
+
     soup = BeautifulSoup(html_doc, 'html.parser')
     b_tag = soup.find_all("table")
     trs = b_tag[0].find_all("tr")
@@ -40,6 +35,7 @@ def find_closing_price_TPEX(stock_number, year, month):
         tds = tr.find_all("td")
         day_closing_price.append(float(tds[6].text))
     average_month_price = sum(day_closing_price) / len(day_closing_price)
+    average_month_price = round(average_month_price,2)
     # print(average_month_price)
     return average_month_price
 
@@ -70,12 +66,45 @@ def debt_only_cal(debt):
         initial = initial * (1+debt_change)
     return initial
 
+class DB_Operation:
+    def __init__(self, table_name):
+        self.conn = sqlite3.connect('test.db')
+        self.table_name = str(table_name) 
+        
+    def create_table(self):
+        c = self.conn.cursor()
+        str = '''CREATE TABLE IF NOT EXISTS {table_name}
+        (ID INTEGER PRIMARY KEY  AUTOINCREMENT   NOT NULL,
+        Y_M              TEXT   NOT NULL,
+        price           FLOAT    NOT NULL);'''.format(table_name="[" + self.table_name + "]")
+        c.execute(str)
+        self.conn.commit()
+    
+    def insert_data(self, Y_M, price):
+        c = self.conn.cursor()
+        str = '''SELECT * FROM {table_name} WHERE Y_M="{Y_M}";'''.format(table_name="[" + self.table_name + "]", Y_M= Y_M)
+        c.execute(str)
+        rows = c.fetchall()
+        if len(rows) == 0:
+            c.execute('''INSERT INTO {table_name} (Y_M,price) \
+            VALUES ("{Y_M}",{price})'''.format(table_name="[" + self.table_name + "]", Y_M= Y_M, price = price))
+        self.conn.commit()
+
+    def connnect_close(self):
+        self.conn.close()
+
+
 if __name__ == "__main__":
     # stock_number = input("stock_number: ")
     initial_money = 1000
     year = 107
     stock = []
     debt = []
+    DB_debt = DB_Operation("00694B")
+    DB_debt.create_table()
+    DB_stock = DB_Operation("0050")
+    DB_stock.create_table()
+    
 
     for i in range(year, 110+1):
         for j in range(1, 13):
@@ -85,10 +114,14 @@ if __name__ == "__main__":
             print(month_str)
             average_month_price = find_closing_price_TWSE("0050", year_str, month_str)
             stock.append(float(average_month_price))
+            DB_stock.insert_data(str(i)+"_"+month_str, float(average_month_price))
+            
             year_str = str(year)
             month_str = str(j).zfill(2)
             average_month_price = find_closing_price_TPEX("00694B",year_str,month_str)
             debt.append(float(average_month_price))
+            DB_debt.insert_data(str(i)+"_"+month_str, float(average_month_price))
+
     dual_result = dual_momentum(stock, debt)
     stock_only = stock_only_cal(stock)
     debt_only = debt_only_cal(debt)
